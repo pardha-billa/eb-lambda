1c48f849995ed5effb08f3057bee7ba411114f2c
Pramod <pramod@digitalthrottle.com>
2022-08-10 22:36:08 +0530

    removed raw credentials

--- app/services/aws_storage.rb
+++ app/services/aws_storage.rb
@@ -3,8 +3,7 @@ class AwsStorage
     def s3_connection
       Aws.config.update({
         region: 'us-east-1',
-        #credentials: Aws::Credentials.new("#{Rails.application.credentials.aws_key}", "#{Rails.application.credentials.aws_secret}")
-        credentials: Aws::Credentials.new('AKIAXYVO7HIGKQGDNDRW','/N4wAgIAcypGeg5pS8zCqxVvxXKvmgkE1fQqsDT3')
+        credentials: Aws::Credentials.new("#{Rails.application.credentials.aws_key}", "#{Rails.application.credentials.aws_secret}")
       })
     end
 end
